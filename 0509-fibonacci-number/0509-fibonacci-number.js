db5d5558543adc0301903b392968c3f7ce56a26b
Sarvarbek Sharipboyev <sarvarbek.sharipboyev1@s.amity.edu>
2026-01-21 22:14:06 +0500

    Time: 33 ms (97.38%), Space: 52.8 MB (93.93%) - LeetHub

---
+++ 0509-fibonacci-number/0509-fibonacci-number.js
@@ -0,0 +1,16 @@
+/**
+ * @param {number} n
+ * @return {number}
+ */
+var fib = function(n) {
+    if(n===0) return 0;
+    if (n===1) return 1;
+    let prev2 = 0;
+    let prev1 = 1;
+    for (let i = 2; i<=n; i++){
+      let current = prev1 +prev2
+        prev2=prev1
+        prev1=current
+    }
+    return prev1;
+};
